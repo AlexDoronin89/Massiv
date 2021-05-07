fd1a58610388724dce59287efc70a6ee641decb8
adoronin158@gmail.com <adoronin158@gmail.com>
2021-05-07 14:26:34 +0300

    agh

---
+++ massiv/impossible lvl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impossible_lvl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masiv = { 2, 1, 3, 5 };
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                Console.Write($"{masiv[i]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ massiv/massiv/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] year =new int[30];
+            int min=int.MaxValue ,  max = int.MinValue;
+            for (int i = 0; i < year.Length; i++)
+            {
+                year[i] = random.Next(1950 , 2020 + 1);
+                Console.Write($"{year[i]} ");
+                if(year[i]> max)
+                {
+                    max = year[i];
+                }
+                if(min > year[i])
+                {
+                    min = year[i];
+                }
+            }
+            
+            
+                Console.WriteLine($"youngest {max}, oldest {min} ");
+            
+
+
+            Console.ReadKey();
+        } 
+        
+    }
+    
+}
